a19a4dd5ac4da9da51cce7f33969873e4e221bed
itscopebd <itscopebd@gmail.com>
2023-05-03 16:06:18 +0600

    newsletter section added

---
+++ src/components/newsletter/Newsletter.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./newsletter.css"
+const Newsletter = () => {
+    return (
+
+        <div className='subscribe-wrapper'>
+            <div className='subscribe'>
+
+                <div className='container flex mx-auto flex-col md:flex-row md:px-0 px-1'>
+                    <div className='w-12/12 md:w-6/12 mb-4 text-center md:text-left md:mb-0'>
+                        <h4 class="subs-title">Stay Informed About Special Offers</h4>
+                        <p className='subs-des'>For Exclusive Deals, Coupons, News and More!</p>
+                    </div>
+                    <div className='w-12/12 md:w-6/12'>
+                        <div className="form-control">
+                            <div className="input-group">
+                                <input type="text" placeholder="Search…" className="input input-bordered" />
+                                <button className="btn btn-square bg-warning text-sm">
+                                 Subscribe
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Newsletter;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../components/banner/Banner';
 import LoadData from '../loadApiData/LoadData';
+import Newsletter from '../components/newsletter/Newsletter';
 
 const HomePage = () => {
     
@@ -10,6 +11,7 @@ const HomePage = () => {
             <div className='container mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-10 my-10'>
                 <LoadData></LoadData>
             </div>
+            <Newsletter></Newsletter>
         </>
     );
 };
